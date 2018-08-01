4f86f34be4339db1570fa9c38f76fea0000bc402
Hiland Hall <hiland@gmail.com>
2018-08-01 13:21:05 -0700

    Working version for GCP.  Working on local machine.

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'kus6snj51nzgdx4g5@d3svf@ysnr3a^_r81ceayyklz=q*n%c6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.159', '76.14.32.33','127.0.0.1','localhost']
-
+#ALLOWED_HOSTS = ['192.168.1.159', '76.14.32.33','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware', #<-- Adds Social Login
 ]
 
 ROOT_URLCONF = 'dictionary.urls'
@@ -89,12 +89,49 @@ WSGI_APPLICATION = 'dictionary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+""" This was the ORIGINAL database used during initial development!
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+# GCP Database Settings
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+# Check to see if MySQLdb is available; if not, have pymysql masquerade as
+# MySQLdb. This is a convenience feature for developers who cannot install
+# MySQLdb locally; when running in production on Google App Engine Standard
+# Environment, MySQLdb will be used.
+try:
+    import MySQLdb  # noqa: F401
+except ImportError:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
+# [START dbconfig]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'devnumreg',
+        'USER': 'dbdude',
+        'PASSWORD': 'H1landR0cks',
+        # For MySQL, set 'PORT': '3306' instead of the following. Any Cloud
+        # SQL Proxy instances running locally must also be set to tcp:3306.
+        'PORT': '3307',
+    }
+}
+# In the flexible environment, you connect to CloudSQL using a unix socket.
+# Locally, you can use the CloudSQL proxy to proxy a localhost connection
+# to the instance
+DATABASES['default']['HOST'] = '/cloudsql/numreg-209816:us-west2:devnumreg'
+if os.getenv('GAE_INSTANCE'):
+    pass
+else:
+    DATABASES['default']['HOST'] = '127.0.0.1'
+# [END dbconfig]
 
 
 # Password validation
@@ -137,5 +174,5 @@ LOGIN_REDIRECT_URL = 'index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
